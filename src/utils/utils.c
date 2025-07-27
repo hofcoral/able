7f33aaf753c0f7d8f1fbb51b4dfa295367e530de
Hof Coral <hofcoral@gmail.com>
2025-07-27 16:59:48 +0900

    [trivial] Update line num log

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -26,7 +26,7 @@ void log_script_error(int line, int column, const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    fprintf(stderr, "[ERROR line %d:%d] ", line, column);
+    fprintf(stderr, "[ERROR in line %d:%d] ", line, column);
     vfprintf(stderr, fmt, args);
     fprintf(stderr, "\n");
     va_end(args);
