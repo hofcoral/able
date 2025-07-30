7774a1b53f45303d82da0622a86adb2ea16a62a6
Hof Coral <73271383+hofcoral@users.noreply.github.com>
2025-07-30 12:12:47 -0400

    [feat] Provide sqrt in standard math module

--- tests/integration/test_imports.py
+++ tests/integration/test_imports.py
@@ -10,5 +10,9 @@ class ImportTests(AbleTestCase):
         output = self.run_script('examples/modules/from_import.abl')
         self.assertEqual(output, '5\n')
 
+    def test_math_sqrt(self):
+        output = self.run_script('examples/modules/import_sqrt.abl')
+        self.assertEqual(output, '3\n')
+
 if __name__ == '__main__':
     unittest.main()
